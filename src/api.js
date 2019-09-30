e0f22628f6be65069437d0527253413047133b00
Zee Celest <zeecelest@gmail.com>
2019-09-30 12:27:03 -0400

    complete step 7

--- src/api.js
+++ src/api.js
@@ -1,24 +1,36 @@
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/lazandrea';
 
-const getItems = function() {
+const getItems = function () {
   return fetch(`${BASE_URL}/items`);
 };
 
-const createItem = function(name) {
+const createItem = function (name) {
   let newItem = JSON.stringify({
     name
   });
 
   return fetch(`${BASE_URL}/items`, {
     'method': 'post',
-    'headers':  {
-        'Content-Type': 'application/json'
-      },
-    'body' : newItem
+    'headers': {
+      'Content-Type': 'application/json'
+    },
+    'body': newItem
+  });
+};
+
+const updateItem = function (id, updateData) {
+  let bodyInfo = JSON.stringify(updateData);
+  fetch(`${BASE_URL}/items/${id}`, {
+    'method': 'PATCH',
+    'header': {
+      'Content-Type': 'application/json'
+    },
+    'body': bodyInfo
   });
 };
 
 export default {
   getItems,
-  createItem
+  createItem,
+  updateItem
 };
