ee7a9cb1ba22e43466a3c59ad7103a7b8086e4b8
Zee Celest <zeecelest@gmail.com>
2019-09-30 11:10:12 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import $ from 'jquery';
-
+import api from './api';
 import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
 
-const main = function () {
+const main = function() {
+  api.getItems().then(res => console.log(res));
+  console.log(api.BASE_URL);
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
